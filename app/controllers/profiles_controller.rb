354b00ca69e111cb4e045399d5d01d4bca08de98
Avivit Weissman <wavivit@gmail.com>
2021-09-22 18:35:18 -0700

    add friend_request + display of list and status

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,6 +12,34 @@ class ProfilesController < ApplicationController
   end
 
   def show
+    # Load user to get user's id from username
     @user = User.find_by! username: params[:username]
+
+    # load both directions of relationship
+    @friendship_requested = Friendship.where(:requester => current_user[:id], :requested => @user.id).first()
+    @friendship_received = Friendship.where(:requester => @user.id, :requested => current_user[:id]).first()
+    
+    # Load actual "Approved" relationships in both directions for displaying list
+    @friends_list_received = Friendship.where(:requested => @user.id, :status => "Approved")
+    @friends_list_requested = Friendship.where(:requester => @user.id, :status => "Approved")
+    
   end
+
+  def friend_request
+
+    # The user we are requesting from
+    @requested = User.find_by! username: params[:username]
+
+    @friend = Friendship.new
+
+    @friend.requester = current_user
+    @friend.requested = @requested
+    @friend.status = "Request" # Request / Approved / Rejected
+    if @friend.save
+      redirect_to [:profiles,params[:username]], notice: 'your friend request sent!'
+    else
+      redirect_to [:profiles], notice: 'something went wrong'
+    end
+  end
+
 end
